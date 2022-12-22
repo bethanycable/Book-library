0531899b4da5d214754280f4553ca001021a28cc
Bethany Mattern <bethany.a.cable@gmail.com>
2022-12-21 22:27:56 -0500

    Rename and modify Book Component, add styling

--- client/components/BookCard.jsx
+++ client/components/BookCard.jsx
@@ -2,23 +2,27 @@ import { useEffect, useState } from 'react';
 
 import React from 'react';
 
-const Book = () => {
+const BookCard = ({id, title}) => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    fetch('/api/readbooks').then((res) => res.json())
+    fetch(`/api/${id}`).then((res) => res.json())
       .then((data) => {
           // console.log(data)
           setBooks(data);
         });
   }, []);
 
-const bookItems = books.map((book, index) => {
+const bookItems = books.map((book) => {
   return <li key={book.book_id}>"{book.book_title}" by {book.first_name} {book.last_name}</li>
 })
-  return (<div>
-    <ul>{bookItems}</ul>
+  return (
+    <div className='book-card'>
+      <h4 id={id}>{title}</h4>
+      <ul>{bookItems}</ul>
+
+      <button className='add-book'>Add a Book</button>
     </div>);
 };
 
-export default Book;
+export default BookCard;
--- client/containers/Main.jsx
+++ client/containers/Main.jsx
@@ -1,10 +1,23 @@
-import Book from '../components/Book.jsx'
+import BookCard from '../components/BookCard.jsx'
 import React from 'react'
 
 const Main = () => {
+
+  const bookCards = [
+    { id:'readbooks', title: 'Read Books'}, 
+    {id:'likedbooks', title: 'Liked Books'}, 
+    {id: 'dislikedbooks', title: 'Disliked Books'}, 
+    {id: 'toreadbooks', title: 'Books to Read'}].map((call, index) => {
+    return <BookCard key={index} id={call.id} title={call.title} />
+  })
+  console.log(bookCards)
+
   return (
     <div className='book-section'>
-      <Book />
+      <h3>All Books</h3>
+      <div className='card-container'>
+        {bookCards}
+      </div>
     </div>
     
   )
